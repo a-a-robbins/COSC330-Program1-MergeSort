0151744426e73b8b377f9d51da12655f35adefd2
a-a-robbins <a.a.robbins@outlook.com>
2022-09-13 20:56:15 -0500

    Update MergeSort.java
    added to merge sort function

--- MergeSort.java
+++ MergeSort.java
@@ -15,17 +15,48 @@ public class MergeSort {
    
    }
 
-   public static void mergesort(int[] A) {
+   public static void mergesort(int[] A, int left, int right) {
    
+     if(left >= right) {
+      return; 
+     }
      
+     int mid = (left + right) / 2; 
+     
+     //TEST: print mid
+     System.out.println("mid = " + mid); 
+     
+     int[] leftArray = new int[mid - left]; 
+     int[] rightArray = new int[right - (mid + 1)];
+     
+     for(int i = 0; i < mid; i++) {
+      leftArray[i] = A[i];
+     }
+     
+     for(int j = mid; j < right; j++) {
+      rightArray[j] = A[j]; 
+     }
+     
+     
+     //TEST: did the arrays split properly? 
+      System.out.println("Left Array: "); 
+      for(int i = 0; i < leftArray.length; i++) {
+         System.out.print(leftArray[i] + ", "); 
+      }
+      System.out.println(); 
+      
+        
+   
+          
+     mergesort(A, left, mid); 
+     mergesort(A, (mid + 1), right); 
+     
+     //merge(A, left, mid, right); 
    
    }
    
-   private static void sort(int left, int right) {
    
-   }
-   
-   private static void merge(){
+   private static void merge(int[] A, int left, int mid, int right) {
    
    }
 
@@ -40,8 +71,10 @@ public class MergeSort {
       //TEST print our array to be merge-sorted
       System.out.println("TEST: print array mergeSortMe");
       for(int i = 0; i < mergeSortMe.length; i++) {
-         System.out.print(mergeSortMe[i] + ", ");
+         System.out.println(mergeSortMe[i] + ", ");
       }
+      
+      mergesort(mergeSortMe, 0, (mergeSortMe.length - 1)); 
          
    }
    
