b2fe84472f945923533862ab82b1d86abb8899b4
a-a-robbins <a.a.robbins@outlook.com>
2022-09-15 06:55:03 -0500

    from Trong - sort working

--- MergeSort.java
+++ MergeSort.java
@@ -10,23 +10,70 @@ public class MergeSort {
    public static void generateArray(int[] numbers) {
          
       for(int i = 0; i < numbers.length; i++) {
-         numbers[i] = rand.nextInt(); 
+         numbers[i] = rand.nextInt(1000); 
       }
    
    }
 
+   private static void sort(int[] A, int left, int right) {
+      if (left >= right) {
+         return;
+      }
+      
+      int mid = (left + right)/ 2;
+
+      sort(A, left, mid);
+      sort(A,mid + 1, right);
+      merge(A, left, mid, right);
+   }
+
    public static void mergesort(int[] A) {
-   
-     
-   
+      sort(A, 0, A.length -1);
    }
    
-   private static void sort(int left, int right) {
    
-   }
-   
-   private static void merge(){
-   
+   private static void merge(int[] A, int left, int mid, int right){
+      
+      int leftLength = mid - left +1;
+      int rightLength = right - mid;
+
+      int[] leftArray = new int [leftLength];
+      int[] rightArray = new int[rightLength];
+
+      for (int i = 0; i <= leftLength -1; i ++) {
+         leftArray[i] = A[left + i];
+      }
+
+      for (int j = 0; j <= rightLength - 1; j++){
+         rightArray[j] = A[mid + j +1];
+      }
+
+      int i = 0;
+      int j = 0;
+      int k = left;
+
+      while (i < leftLength && j < rightLength){
+         if (leftArray[i] <= rightArray[j]){
+            A[k] = leftArray[i];
+            i = i +1;
+         }else {
+            A[k] = rightArray[j];
+            j = j +1;
+         }
+         k = k + 1;
+      }
+
+      while (i < leftLength) {
+         A[k] = leftArray[i];
+         i = i + 1;
+         k = k + 1;
+      }
+
+      while(j < rightLength) {
+         A[k] = rightArray[j];
+         j = j +1;
+         k = k +1;
+      }
    }
 
 
@@ -36,7 +83,7 @@ public class MergeSort {
              
       int[] mergeSortMe = new int[size]; 
       generateArray(mergeSortMe);
-      
+      mergesort(mergeSortMe);
       //TEST print our array to be merge-sorted
       System.out.println("TEST: print array mergeSortMe");
       for(int i = 0; i < mergeSortMe.length; i++) {
@@ -46,6 +93,3 @@ public class MergeSort {
    }
    
 }
-
-
-
